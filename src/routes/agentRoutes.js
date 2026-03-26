1133a0bcc6d39473bbe415cd25d4da419fd2dabe
Kollepara Venkata Sri Chakravarthi <vschakrvarthi7@gmail.com>
2026-03-26 12:06:43 +0530

    debug: add test route to agent group

--- src/routes/agentRoutes.js
+++ src/routes/agentRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { runAgent } = require('../controllers/agentController');
 
 router.post('/run', runAgent);
+router.get('/test', (req, res) => res.json({ message: 'Agent API is working' }));
 
 module.exports = router;
